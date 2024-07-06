570029efc3acaa41ee7b8ca91dca86ec6b508618
Habiburrohman89 <kenzoohabib@gmail.com>
2024-07-06 11:13:56 +0700

    edit serializers di task dan edit di settings

--- Tasks/serializers.py
+++ Tasks/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Tugas
 
 class TugasSerializer(serializers.ModelSerializer):
+    batas_waktu = serializers.DateField(format='%Y-%m-%d')
+
     class Meta:
         model = Tugas
-        fields = ('id', 'judul', 'deskripsi', 'pembuat','assigned_to', 'kategori', 'tanggal_dibuat', ' batas_waktu')
+        fields = ('id', 'judul', 'deskripsi', 'pembuat','assigned_to', 'tanggal_dibuat', 'batas_waktu')
